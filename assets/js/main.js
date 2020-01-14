18b0d8b72de388af64b811603f2885f7936e87ec
John Høj Andreassen <john@techcare.of>
2020-01-14 10:47:01 +0000

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,24 +47,24 @@
     });
 
   // Header.
-  if ($banner.length > 0 && $header.hasClass("alt")) {
-    $window.on("resize", function() {
-      $window.trigger("scroll");
-    });
+  // if ($banner.length > 0 && $header.hasClass("alt")) {
+  //   $window.on("resize", function() {
+  //     $window.trigger("scroll");
+  //   });
 
-    $banner.scrollex({
-      bottom: $header.outerHeight(),
-      terminate: function() {
-        $header.removeClass("alt");
-      },
-      enter: function() {
-        $header.addClass("alt");
-      },
-      leave: function() {
-        $header.removeClass("alt");
-      }
-    });
-  }
+  //   $banner.scrollex({
+  //     bottom: $header.outerHeight(),
+  //     terminate: function() {
+  //       $header.removeClass("alt");
+  //     },
+  //     enter: function() {
+  //       $header.addClass("alt");
+  //     },
+  //     leave: function() {
+  //       $header.removeClass("alt");
+  //     }
+  //   });
+  // }
 
   // Images.
   $(".image[data-position]").each(function() {
